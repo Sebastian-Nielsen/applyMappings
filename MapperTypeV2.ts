80a1b9b174209e3a02fbcf78ea28383ac15b21b0
sebastian <sebastiangrundet@hotmail.dk>
2024-12-10 18:53:44 +0100

    add the error to the onError option

--- MapperTypeV2.ts
+++ MapperTypeV2.ts
@@ -55,35 +55,35 @@ type ABCDEFG<A,B,C,D,E,F,G> = [
 ]
 
 export function applyMappings<A>(variable: A, mappings: _A<A>): void; // mappings is empty
-export function applyMappings<A,B>(variable: A, mappings: AB<A,B>, options: {onError: () => B}): B; // mappings.length==1
+export function applyMappings<A,B>(variable: A, mappings: AB<A,B>, options: {onError: (e: Err) => B}): B; // mappings.length==1
 export function applyMappings<A,B>(variable: A, mappings: AB<A,B>, options: {onError: undefined}): IncludeErr<B, [B]>; // mappings.length==1
 export function applyMappings<A,B>(variable: A, mappings: AB<A,B>, options: {}): IncludeErr<B, [B]>; // mappings.length==1
 export function applyMappings<A,B>(variable: A, mappings: AB<A,B>): IncludeErr<B, [B]>; // mappings.length==1
-export function applyMappings<A,B,C>(variable: A, mappings: ABC<A,B,C>, options: {onError: () => C}): C;
+export function applyMappings<A,B,C>(variable: A, mappings: ABC<A,B,C>, options: {onError: (e: Err) => C}): C;
 export function applyMappings<A,B,C>(variable: A, mappings: ABC<A,B,C>, options: {onError: undefined}): IncludeErr<C,[B,C]>;
 export function applyMappings<A,B,C>(variable: A, mappings: ABC<A,B,C>, options: {}): IncludeErr<C,[B,C]>;
 export function applyMappings<A,B,C>(variable: A, mappings: ABC<A,B,C>): IncludeErr<C,[B,C]>;
-export function applyMappings<A,B,C,D>(variable: A, mappings: ABCD<A,B,C,D>, options: {onError: () => D}): D;
+export function applyMappings<A,B,C,D>(variable: A, mappings: ABCD<A,B,C,D>, options: {onError: (e: Err) => D}): D;
 export function applyMappings<A,B,C,D>(variable: A, mappings: ABCD<A,B,C,D>, options: {}): IncludeErr<D,[B,C,D]>;
 export function applyMappings<A,B,C,D>(variable: A, mappings: ABCD<A,B,C,D>, options: undefined): IncludeErr<D,[B,C,D]>;
 export function applyMappings<A,B,C,D>(variable: A, mappings: ABCD<A,B,C,D>): IncludeErr<D,[B,C,D]>; // mappings.length==3
-export function applyMappings<A,B,C,D,E>(variable: A, mappings: ABCDE<A,B,C,D,E>, options: {onError: () => E}): E;
+export function applyMappings<A,B,C,D,E>(variable: A, mappings: ABCDE<A,B,C,D,E>, options: {onError: (e: Err) => E}): E;
 export function applyMappings<A,B,C,D,E>(variable: A, mappings: ABCDE<A,B,C,D,E>, options: {}): IncludeErr<E,[B,C,D,E]>;
 export function applyMappings<A,B,C,D,E>(variable: A, mappings: ABCDE<A,B,C,D,E>, options: undefined): IncludeErr<E,[B,C,D,E]>;
 export function applyMappings<A,B,C,D,E>(variable: A, mappings: ABCDE<A,B,C,D,E>): IncludeErr<E,[B,C,D,E]>;
-export function applyMappings<A,B,C,D,E,F>(variable: A, mappings: ABCDEF<A,B,C,D,E,F>, options: {onError: () => F}): F; // mappings.length==5
+export function applyMappings<A,B,C,D,E,F>(variable: A, mappings: ABCDEF<A,B,C,D,E,F>, options: {onError: (e: Err) => F}): F; // mappings.length==5
 export function applyMappings<A,B,C,D,E,F>(variable: A, mappings: ABCDEF<A,B,C,D,E,F>, options: {onError: undefined}): IncludeErr<F,[B,C,D,E,F]>; // mappings.length==5
 export function applyMappings<A,B,C,D,E,F>(variable: A, mappings: ABCDEF<A,B,C,D,E,F>, options: {}): IncludeErr<F,[B,C,D,E,F]>; // mappings.length==5
 export function applyMappings<A,B,C,D,E,F>(variable: A, mappings: ABCDEF<A,B,C,D,E,F>, options: undefined): IncludeErr<F,[B,C,D,E,F]>; // mappings.length==5
 export function applyMappings<A,B,C,D,E,F>(variable: A, mappings: ABCDEF<A,B,C,D,E,F>): IncludeErr<F,[B,C,D,E,F]>; // mappings.length==5
-export function applyMappings<A,B,C,D,E,F,G>(variable: A, mappings: ABCDEFG<A,B,C,D,E,F,G>, options: {onError: () => G}): G;
+export function applyMappings<A,B,C,D,E,F,G>(variable: A, mappings: ABCDEFG<A,B,C,D,E,F,G>, options: {onError: (e: Err) => G}): G;
 export function applyMappings<A,B,C,D,E,F,G>(variable: A, mappings: ABCDEFG<A,B,C,D,E,F,G>, options: {onError: undefined}): IncludeErr<G,[B,C,D,E,F,G]>;
 export function applyMappings<A,B,C,D,E,F,G>(variable: A, mappings: ABCDEFG<A,B,C,D,E,F,G>, options: {}): IncludeErr<G,[B,C,D,E,F,G]>;
 export function applyMappings<A,B,C,D,E,F,G>(variable: A, mappings: ABCDEFG<A,B,C,D,E,F,G>): IncludeErr<G,[B,C,D,E,F,G]>;
 export function applyMappings(variable: any, mappings: any, options?: Record<any, any>): any {
 	if (variable instanceof Err) {
 		if (options?.onError !== undefined) {
-			return options.onError();
+			return options.onError(variable);
 		}
 		return variable;
 	}
@@ -143,35 +143,35 @@ type _ABCDEFG<A,B,C,D,E,F,G> = [
 	(f: Exclude<F,Err>) => G | Promise<G>,
 ]
 
-export async function applyMappingsAsync<A>(variable: A, mappings: _A<A>, options: {onError?: () => any}): Promise<void>; // mappings is empty
-export async function applyMappingsAsync<A,B>(variable: A, mappings: _AB<A,B>, options: {onError: () => B}): Promise<B>; // mappings.length==1
+export async function applyMappingsAsync<A>(variable: A, mappings: _A<A>, options: {onError?: (e: Err) => any}): Promise<void>; // mappings is empty
+export async function applyMappingsAsync<A,B>(variable: A, mappings: _AB<A,B>, options: {onError: (e: Err) => B}): Promise<B>; // mappings.length==1
 export async function applyMappingsAsync<A,B>(variable: A, mappings: _AB<A,B>, options: {onError: undefined}): Promise<IncludeErr<B, [B]>>; // mappings.length==1
 export async function applyMappingsAsync<A,B>(variable: A, mappings: _AB<A,B>, options: {}): Promise<IncludeErr<B, [B]>>; // mappings.length==1
 export async function applyMappingsAsync<A,B>(variable: A, mappings: _AB<A,B>): Promise<IncludeErr<B, [B]>>; // mappings.length==1
-export async function applyMappingsAsync<A,B,C>(variable: A, mappings: _ABC<A,B,C>, options: {onError: () => C}): Promise<C>;
+export async function applyMappingsAsync<A,B,C>(variable: A, mappings: _ABC<A,B,C>, options: {onError: (e: Err) => C}): Promise<C>;
 export async function applyMappingsAsync<A,B,C>(variable: A, mappings: _ABC<A,B,C>, options: {onError: undefined}): Promise<IncludeErr<C,[B,C]>>;
 export async function applyMappingsAsync<A,B,C>(variable: A, mappings: _ABC<A,B,C>, options: undefined): Promise<IncludeErr<C,[B,C]>>;
 export async function applyMappingsAsync<A,B,C>(variable: A, mappings: _ABC<A,B,C>): Promise<IncludeErr<C,[B,C]>>;
-export async function applyMappingsAsync<A,B,C,D>(variable: A, mappings: _ABCD<A,B,C,D>, options: {onError: () => D}): Promise<D>; // mappings.length==3
+export async function applyMappingsAsync<A,B,C,D>(variable: A, mappings: _ABCD<A,B,C,D>, options: {onError: (e: Err) => D}): Promise<D>; // mappings.length==3
 export async function applyMappingsAsync<A,B,C,D>(variable: A, mappings: _ABCD<A,B,C,D>, options: {onError: undefined}): Promise<IncludeErr<D,[B,C,D]>>; // mappings.length==3
 export async function applyMappingsAsync<A,B,C,D>(variable: A, mappings: _ABCD<A,B,C,D>, options: undefined): Promise<IncludeErr<D,[B,C,D]>>; // mappings.length==3
 export async function applyMappingsAsync<A,B,C,D>(variable: A, mappings: _ABCD<A,B,C,D>): Promise<IncludeErr<D,[B,C,D]>>; // mappings.length==3
-export async function applyMappingsAsync<A,B,C,D,E>(variable: A, mappings: _ABCDE<A,B,C,D,E>, options: {onError: () => E}): Promise<E>;
+export async function applyMappingsAsync<A,B,C,D,E>(variable: A, mappings: _ABCDE<A,B,C,D,E>, options: {onError: (e: Err) => E}): Promise<E>;
 export async function applyMappingsAsync<A,B,C,D,E>(variable: A, mappings: _ABCDE<A,B,C,D,E>, options: {onError: undefined}): Promise<IncludeErr<E,[B,C,D,E]>>;
 export async function applyMappingsAsync<A,B,C,D,E>(variable: A, mappings: _ABCDE<A,B,C,D,E>, options: undefined): Promise<IncludeErr<E,[B,C,D,E]>>;
 export async function applyMappingsAsync<A,B,C,D,E>(variable: A, mappings: _ABCDE<A,B,C,D,E>): Promise<IncludeErr<E,[B,C,D,E]>>;
-export async function applyMappingsAsync<A,B,C,D,E,F>(variable: A, mappings: _ABCDEF<A,B,C,D,E,F>, options: {onError: () => F}): Promise<F>; // mappings.length==5
+export async function applyMappingsAsync<A,B,C,D,E,F>(variable: A, mappings: _ABCDEF<A,B,C,D,E,F>, options: {onError: (e: Err) => F}): Promise<F>; // mappings.length==5
 export async function applyMappingsAsync<A,B,C,D,E,F>(variable: A, mappings: _ABCDEF<A,B,C,D,E,F>, options: {onError: undefined}): Promise<IncludeErr<F,[B,C,D,E,F]>>; // mappings.length==5
 export async function applyMappingsAsync<A,B,C,D,E,F>(variable: A, mappings: _ABCDEF<A,B,C,D,E,F>, options: undefined): Promise<IncludeErr<F,[B,C,D,E,F]>>; // mappings.length==5
 export async function applyMappingsAsync<A,B,C,D,E,F>(variable: A, mappings: _ABCDEF<A,B,C,D,E,F>): Promise<IncludeErr<F,[B,C,D,E,F]>>; // mappings.length==5
-export async function applyMappingsAsync<A,B,C,D,E,F,G>(variable: A, mappings: _ABCDEFG<A,B,C,D,E,F,G>, options: {onError: () => G}): Promise<G>;
+export async function applyMappingsAsync<A,B,C,D,E,F,G>(variable: A, mappings: _ABCDEFG<A,B,C,D,E,F,G>, options: {onError: (e: Err) => G}): Promise<G>;
 export async function applyMappingsAsync<A,B,C,D,E,F,G>(variable: A, mappings: _ABCDEFG<A,B,C,D,E,F,G>, options: {onError: undefined}): Promise<IncludeErr<G,[B,C,D,E,F,G]>>;
 export async function applyMappingsAsync<A,B,C,D,E,F,G>(variable: A, mappings: _ABCDEFG<A,B,C,D,E,F,G>, options: undefined): Promise<IncludeErr<G,[B,C,D,E,F,G]>>;
 export async function applyMappingsAsync<A,B,C,D,E,F,G>(variable: A, mappings: _ABCDEFG<A,B,C,D,E,F,G>): Promise<IncludeErr<G,[B,C,D,E,F,G]>>;
 export async function applyMappingsAsync(variable: any, mappings: any, options?: Record<any, any>): Promise<any> {
 	if (variable instanceof Err) {
 		if (options?.onError !== undefined) {
-			return options.onError();
+			return options.onError(variable);
 		}
 		return variable;
 	}
